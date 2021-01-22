2823312982a209708b091c60005999f6a97ae7a7
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-22 11:27:50 +0530

    [Saiprasad]Added: printing hello from bridgelabz when name is given and also validated the name

--- src/App.js
+++ src/App.js
@@ -8,19 +8,26 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      userName: ""
+      userName: "",
+      nameError: ""
     };
   }
   // onClick function
-  onClick = (event) => {
+  onclick = (event) => {
     console.log("save button is clicked ", { event });
-    window.open(this.url);
+    window.open(this.url, "_blank");
   };
   //onChange Event function
   onNameChangeHandler = (event) => {
     console.log("value is ", event.target.value);
     // see the title using setState Method
+    const nameRegex = RegExp("^[A-Z]{1}[a-z\\s]{2,}$");
     this.setState({ userName: event.target.value });
+    if (nameRegex.test(event.target.value)) {
+      this.setState({ nameError: `` });
+    } else {
+      this.setState({ nameError: `Name is incorrect` });
+    }
   };
   render() {
     return (
@@ -28,12 +35,14 @@ class App extends React.Component {
         <div>
           <h1>Hello {this.state.userName} from BridgeLabz</h1>
           <img
-            src={logo} onClick={this.onClick}
+            src={logo}
+            onClick={this.onClick}
             alt="The BridgeLabz logo: A Bridge to Employement through lab works"
           />
         </div>
         <div className="text-box">
           <input onChange={this.onNameChangeHandler} />
+          <span className="error-output">{this.state.nameError}</span>
         </div>
       </div>
     );
