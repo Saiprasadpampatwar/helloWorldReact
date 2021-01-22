3684df6da1e668c8a81801d39b290bd94269df3d
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-22 11:03:07 +0530

    [Saiprasad]Added: Launch url on clicking images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import logo from '../src/assets/images/logo.jpg'
+import React from "react";
+
+class App extends React.Component {
+  url = "https://www.bridgelabz.com/";
+  constructor() {
+    super();
+    this.state = {
+      title: "HelloWorld Demo App  from Bridgelabz !!"
+    };
+  }
+   // onClick function
+  clickOnBL_Page = (e) => {
+    console.log("Click event works",e)
+    window.open(this.url)
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <img
+          src={logo} onClick ={this.clickOnBL_Page}
+          alt="The BridgeLabz logo: A Bridge to Employement through lab works"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
