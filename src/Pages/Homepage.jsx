9603e13a3df54ed437284177c880ff48778f3139
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-22 12:12:46 +0530

    [Saiprasad]Added: full Bridgelabz page

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Home from '../Components/Home'
+
+class Homepage extends Component{
+    render(){
+        <div>
+            <Home/>
+        </div>
+    }
+}
+
+export default Homepage;
