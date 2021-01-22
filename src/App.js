a53f7e9474d90119c5eb59aec62fce3ef18feb69
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2021-01-22 11:22:48 +0530

    [Saiprasad]Added: printing hello from bridgelabz when name is given

--- src/App.js
+++ src/App.js
@@ -8,22 +8,33 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "HelloWorld Demo App  from Bridgelabz !!"
+      userName: ""
     };
   }
-   // onClick function
-  clickOnBL_Page = (e) => {
-    console.log("Click event works",e)
-    window.open(this.url)
-  }
+  // onClick function
+  onClick = (event) => {
+    console.log("save button is clicked ", { event });
+    window.open(this.url);
+  };
+  //onChange Event function
+  onNameChangeHandler = (event) => {
+    console.log("value is ", event.target.value);
+    // see the title using setState Method
+    this.setState({ userName: event.target.value });
+  };
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <img
-          src={logo} onClick ={this.clickOnBL_Page}
-          alt="The BridgeLabz logo: A Bridge to Employement through lab works"
-        />
+        <div>
+          <h1>Hello {this.state.userName} from BridgeLabz</h1>
+          <img
+            src={logo} onClick={this.onClick}
+            alt="The BridgeLabz logo: A Bridge to Employement through lab works"
+          />
+        </div>
+        <div className="text-box">
+          <input onChange={this.onNameChangeHandler} />
+        </div>
       </div>
     );
   }
